aa923c466316daf8e65fb739d342973ce4b48e0c
Raphael-Soares <soaresphaell@gmail.com>
2026-04-02 13:52:17 -0300

    feat: add marks.nvim plugin for better navigation

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,16 +1,11 @@
 return {
     "stevearc/oil.nvim",
-    dependencies = {
-        { "nvim-mini/mini.icons", opts = {} },
-    },
+    dependencies = { "nvim-tree/nvim-web-devicons" },
     keys = {
         { "-", "<cmd>Oil<CR>", desc = "Abrir diretório pai (Oil)" },
     },
     config = function()
         require("oil").setup({
-            win_options = {
-                signcolumn = "yes:2",
-            },
             keymaps = {
                 ["g?"] = "actions.show_help",
                 ["<CR>"] = "actions.select",
@@ -19,34 +14,6 @@ return {
                 ["<C-p>"] = "actions.preview",
                 ["q"] = "actions.close",
                 ["g."] = "actions.toggle_hidden",
-            },
-        })
-
-        -- 2. Setup do Git Status com ícones informativos
-        require("oil-git-status").setup({
-            symbols = {
-                index = {
-                    ["!"] = "󰛑", -- Ignored
-                    ["?"] = "", -- Untracked
-                    ["A"] = "", -- Added
-                    ["C"] = "󰆏", -- Copied
-                    ["D"] = "", -- Deleted
-                    ["M"] = "", -- Modified
-                    ["R"] = "", -- Renamed
-                    ["T"] = "󰏫", -- Type changed
-                    ["U"] = "", -- Unmerged
-                },
-                working_tree = {
-                    ["!"] = "",
-                    ["?"] = "",
-                    ["A"] = "",
-                    ["C"] = "󰆏",
-                    ["D"] = "",
-                    ["M"] = "",
-                    ["R"] = "",
-                    ["T"] = "󰏫",
-                    ["U"] = "",
-                },
             },
         })
     end,
